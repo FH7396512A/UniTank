1f0769070e4d44724c38f06c12f489a092a74976
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2021-11-09 02:47:11 +0900

    로그인 회원가입 구현 및 예외처리

--- Assets/codes/PlayFabManager.cs
+++ Assets/codes/PlayFabManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
@@ -7,26 +8,56 @@ using UnityEngine.UI;
 
 public class PlayFabManager : MonoBehaviour
 {
-    public InputField ID_Input, PW_Input, Name_Input;
+    public InputField ID_Input, PW_Input, Create_ID_Input, Create_PW_Input, Create_Name_Input;
+
+    string patternID = @"^[\w.%+\-]+@[\w.\-]+\.[A-Za-z]{2,3}$";
+    string patternPW = @"^[0-9a-zA-Z!@#$%&*]{6,20}$";
+    string patternName = @"^[0-9a-zA-Z]{2,8}$";
+
     public void Login_Button()
     {
         var request = new LoginWithEmailAddressRequest { Email = ID_Input.text, Password = PW_Input.text };
-        PlayFabClientAPI.LoginWithEmailAddress(request, (result) => Debug.Log("로그인 성공"), OnLoginFailure);
+        if (Regex.IsMatch(ID_Input.text, patternID) && Regex.IsMatch(PW_Input.text, patternPW))
+        {
+            PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+        }
+        else
+        {
+            Debug.LogError("로그인 실패");
+            if (!Regex.IsMatch(ID_Input.text, patternID)) Debug.LogError("아이디 오류");
+            if (!Regex.IsMatch(PW_Input.text, patternPW)) Debug.LogError("패스워드 오류");
+        }
     }
     public void CreatID_Button()
     {
-        var request = new RegisterPlayFabUserRequest { Email = ID_Input.text, Password = PW_Input.text, Username = Name_Input.text };
-        PlayFabClientAPI.RegisterPlayFabUser(request, (result) => Debug.Log("회원가입 성공"), OnRegisterFailure);
+        if (Regex.IsMatch(Create_ID_Input.text, patternID) && Regex.IsMatch(Create_PW_Input.text, patternPW) && Regex.IsMatch(Create_Name_Input.text, patternName))
+        {
+            var request = new RegisterPlayFabUserRequest { Email = Create_ID_Input.text, Password = Create_PW_Input.text, Username = Create_Name_Input.text };
+            PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnRegisterFailure);
+        }
+        else
+        {
+            Debug.LogError("회원가입 실패");
+            if (!Regex.IsMatch(Create_ID_Input.text, patternID)) Debug.LogError("아이디 오류");
+            if (!Regex.IsMatch(Create_PW_Input.text, patternPW)) Debug.LogError("패스워드 오류");
+            if (!Regex.IsMatch(Create_Name_Input.text, patternName)) Debug.LogError("닉네임 오류");
+        }
     }
 
+    private void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("회원가입 성공");
+    }
     private void OnRegisterFailure(PlayFabError error)
     {
         Debug.LogError("회원가입 실패");
-        Debug.LogError(error.GenerateErrorReport());
+    }
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("로그인 성공");
     }
     private void OnLoginFailure(PlayFabError error)
     {
         Debug.LogError("로그인 실패");
-        Debug.LogError(error.GenerateErrorReport());
     }
 }
