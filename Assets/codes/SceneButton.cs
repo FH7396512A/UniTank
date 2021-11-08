590eea060e1ec17a19fe9ceff1a84d3d12193b96
FH7396512A <83833075+FH7396512A@users.noreply.github.com>
2021-11-08 22:20:42 +0100

    파괴되는 지형(미완)
    한게 딱히 없는것같네요...

---
+++ Assets/codes/SceneButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButton : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        //임시로 만들어놓은 테스트코드
+        SceneManager.LoadScene("SelectMenu");
+    }
+}
