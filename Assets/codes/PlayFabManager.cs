0a0a59e421e7c0e13274795ef7c401211ce63660
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2021-11-08 09:16:23 +0900

    PlayFab 적용 및 간단한 로그인 구현

---
+++ Assets/codes/PlayFabManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class PlayFabManager : MonoBehaviour
+{
+    public InputField ID_Input, PW_Input, Name_Input;
+    public void Login_Button()
+    {
+        var request = new LoginWithEmailAddressRequest { Email = ID_Input.text, Password = PW_Input.text };
+        PlayFabClientAPI.LoginWithEmailAddress(request, (result) => Debug.Log("로그인 성공"), OnLoginFailure);
+    }
+    public void CreatID_Button()
+    {
+        var request = new RegisterPlayFabUserRequest { Email = ID_Input.text, Password = PW_Input.text, Username = Name_Input.text };
+        PlayFabClientAPI.RegisterPlayFabUser(request, (result) => Debug.Log("회원가입 성공"), OnRegisterFailure);
+    }
+
+    private void OnRegisterFailure(PlayFabError error)
+    {
+        Debug.LogError("회원가입 실패");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+    private void OnLoginFailure(PlayFabError error)
+    {
+        Debug.LogError("로그인 실패");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
