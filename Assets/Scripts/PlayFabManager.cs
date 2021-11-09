3d8f301a164597a729d76737ec069efde6a7f55c
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2021-11-09 12:03:52 +0900

    로그인 후 채팅 구현

--- Assets/Scripts/PlayFabManager.cs
+++ Assets/Scripts/PlayFabManager.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayFabManager : MonoBehaviour
 {
-    public InputField ID_Input, PW_Input, Create_ID_Input, Create_PW_Input, Create_Name_Input;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public string PlayFabID, DisplayName;
+    public InputField ID_Input, PW_Input, Create_ID_Input, Create_PW_Input, Create_Name_Input, Create_NN_Input;
 
-    string patternID = @"^[\w.%+\-]+@[\w.\-]+\.[A-Za-z]{2,3}$";
-    string patternPW = @"^[0-9a-zA-Z!@#$%&*]{6,20}$";
-    string patternName = @"^[0-9a-zA-Z]{2,8}$";
-
+    string patternID = @"^[\w.%+\-]+@[\w.\-]+\.[A-Za-z]{2,3}$";//이메일 형식
+    string patternPW = @"^[0-9a-zA-Z!@#$%&*]{6,20}$";//패스워드 영어 숫자 특수문자로 구성 6-20자
+    string patternName = @"^[0-9a-zA-Z]{2,20}$";//이름 영어 숫자로 구성 2-20자
+    string patternNN = @"^[0-9a-zA-Z가-힣]{3,8}$";//닉네임 숫자 영어 한글 영어로 구성 3-8자
+    //버튼입력 로그인
     public void Login_Button()
     {
         var request = new LoginWithEmailAddressRequest { Email = ID_Input.text, Password = PW_Input.text };
@@ -28,9 +35,11 @@ public class PlayFabManager : MonoBehaviour
             if (!Regex.IsMatch(PW_Input.text, patternPW)) Debug.LogError("패스워드 오류");
         }
     }
+    //버튼입력 회원가입
     public void CreatID_Button()
     {
-        if (Regex.IsMatch(Create_ID_Input.text, patternID) && Regex.IsMatch(Create_PW_Input.text, patternPW) && Regex.IsMatch(Create_Name_Input.text, patternName))
+        if (Regex.IsMatch(Create_ID_Input.text, patternID) && Regex.IsMatch(Create_PW_Input.text, patternPW) &&
+            Regex.IsMatch(Create_Name_Input.text, patternName) && Regex.IsMatch(Create_NN_Input.text, patternNN))
         {
             var request = new RegisterPlayFabUserRequest { Email = Create_ID_Input.text, Password = Create_PW_Input.text, Username = Create_Name_Input.text };
             PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnRegisterFailure);
@@ -40,13 +49,27 @@ public class PlayFabManager : MonoBehaviour
             Debug.LogError("회원가입 실패");
             if (!Regex.IsMatch(Create_ID_Input.text, patternID)) Debug.LogError("아이디 오류");
             if (!Regex.IsMatch(Create_PW_Input.text, patternPW)) Debug.LogError("패스워드 오류");
-            if (!Regex.IsMatch(Create_Name_Input.text, patternName)) Debug.LogError("닉네임 오류");
+            if (!Regex.IsMatch(Create_Name_Input.text, patternName)) Debug.LogError("이름 오류");
+            if (!Regex.IsMatch(Create_NN_Input.text, patternNN)) Debug.LogError("닉네임 오류");
         }
+    }
+
+    void UpdateDisplayName()
+    {
+        PlayFabClientAPI.UpdateUserTitleDisplayName(new UpdateUserTitleDisplayNameRequest
+        {
+            DisplayName = Create_NN_Input.text
+        }, result => {
+            Debug.Log("The player's display name is now: " + result.DisplayName);
+            DisplayName = result.DisplayName;
+        }, error => Debug.LogError(error.GenerateErrorReport()));
     }
 
     private void OnRegisterSuccess(RegisterPlayFabUserResult result)
     {
         Debug.Log("회원가입 성공");
+        PlayFabID = result.PlayFabID;
+        UpdateDisplayName();
     }
     private void OnRegisterFailure(PlayFabError error)
     {
@@ -55,6 +78,8 @@ public class PlayFabManager : MonoBehaviour
     private void OnLoginSuccess(LoginResult result)
     {
         Debug.Log("로그인 성공");
+        
+        SceneManager.LoadScene("Lobby");
     }
     private void OnLoginFailure(PlayFabError error)
     {
