0b01b70a73b794087b2bb386a5ae55aebf61527e
FH7396512A <83833075+FH7396512A@users.noreply.github.com>
2021-11-23 22:03:06 +0100

    볼륨 조절 UI 부분 완료

--- Assets/Scripts/Lobby_Scene/LobbyBtnControl.cs
+++ Assets/Scripts/Lobby_Scene/LobbyBtnControl.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LobbyBtnControl : MonoBehaviour
 {
     public GameObject CreateBtn, CreateFinishBtn, LobbyPanel, StatusText,
-        CreateRoom;
+        CreateRoom, SoundControlButton, SoundControlBackButton;
+    public Slider VolumeSlider;
+    public AudioSource audio;
+    private float VolumeValue = 1f;
 
     public void CreateBtnClicked()
     {
@@ -15,10 +19,41 @@ public class LobbyBtnControl : MonoBehaviour
         LobbyPanel.gameObject.SetActive(false);
         StatusText.gameObject.SetActive(false);
         CreateRoom.gameObject.SetActive(true);
+        SoundControlButton.gameObject.SetActive(false);
     }
     public void BackButtonOnClicked()
     {
         Debug.Log("Back to Main");
         SceneManager.LoadScene("Main");
+    }
+
+    public void SoundControlOnClicked()
+    {
+        VolumeSlider.gameObject.SetActive(true);
+        SoundControlButton.gameObject.SetActive(false);
+    }
+
+    public void SoundControlBackOnClicked()
+    {
+        VolumeSlider.gameObject.SetActive(false);
+        SoundControlButton.gameObject.SetActive(true);
+    }
+    //볼륨 설정
+    ////////////////////////////////////////////////////////////
+    private void Start()
+    {
+        VolumeValue = PlayerPrefs.GetFloat("VolumeValue", 1f);
+        VolumeSlider.value = VolumeValue;
+        audio.volume = VolumeSlider.value;
+    }
+    void Update()
+    {
+        SoundSlider();
+    }
+    public void SoundSlider()
+    {
+        audio.volume = VolumeSlider.value;
+        VolumeValue = VolumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", VolumeValue);
     }
 }
