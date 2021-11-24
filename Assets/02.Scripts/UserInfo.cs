208294630777854484ab7ae3c18a7e6a723aed4f
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2021-11-25 04:37:00 +0900

    쓸모없는 코드 삭제

--- Assets/02.Scripts/UserInfo.cs
+++ Assets/02.Scripts/UserInfo.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class UserInfo : MonoBehaviour
 {
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
     public static string _DisplayName, _PlayFabID;
     public string GetDisplayName()
     {
